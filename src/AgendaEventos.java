5457a4ed170b84cbf1ce21faf35ac5cd8e71fca9
W Cardoso <cardosu7@gmail.com>
2024-05-05 12:44:00 -0300

    finalizado

--- src/AgendaEventos.java
+++ src/AgendaEventos.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AgendaEventos {
 
@@ -12,8 +13,27 @@ public class AgendaEventos {
     }
 
     public void adicionarEvento(LocalDate data, String nome, String atracao){
-        Evento evento = new Evento(nome, atacao);
+        Evento evento = new Evento(nome, atracao);
         eventoMap.put(data, evento);
     }
 
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        for(Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.APRIL, 20), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.AUGUST, 25), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2025, Month.NOVEMBER, 30), "Evento 4", "Atração 4");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+
     }
+
+
 }
